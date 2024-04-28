bc59dca14f3596cb31daf32fa643854a4cc031e5
Dawar Shamim <DawarShamim1@gmail.com>
2024-04-29 00:54:54 +0500

    updated logger component

--- utils/logger.js
+++ utils/logger.js
@@ -1,27 +1,17 @@
 
 const winston = require('winston');
 require('winston-mongodb');
-const mongoose = require("mongoose");
 
 const { format } = require("winston");
 const { combine, timestamp, json, errors } = winston.format;
 
 require("dotenv").config();
-// MongoDB connection options
+
 const options = {
-  useNewUrlParser: true,
-  useUnifiedTopology: true
+  db: process.env.dbUrl,
+  // options: { useNewUrlParser: true, useUnifiedTopology: true }
+  options: { useUnifiedTopology: true }
 };
-
-const dbUrl = process.env.dbUrl;
-
-mongoose.connect(dbUrl, options)
-  .then(() => {
-    console.log('Logger Connected to Database');
-  })
-  .catch((error) => {
-    console.error('Error connecting to Database:', error);
-  });
 
 
 // Create a logger instance
@@ -33,25 +23,10 @@ const logger = winston.createLogger({
     format.metadata() // >>>> ADD THIS LINE TO STORE the ERR OBJECT IN META field
   ),
   transports: [
-    new winston.transports.MongoDB({
-      db: mongoose.connection, // Pass the MongoDB connection object
-      options: options
-    })
-    // new winston.transports.File({ filename: 'logs/allLogs.log', format: winston.format.json() })
+    new winston.transports.MongoDB(options)
+    // ,new winston.transports.File({ filename: 'logs/allLogs.log', format: winston.format.json() })
   ]
 });
 
-// function logData(key, data) {
-//   try {
-//     logger.info({ [key]: data });
-//   } catch (err) {
-//     logger.error(err);
-//   }
-// }
 
-// logData()
-
-
-
-// module.exports = { logData, logger };
-module.exports = {logger};
+module.exports = { logger };
