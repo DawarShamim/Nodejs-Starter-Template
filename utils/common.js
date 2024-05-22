21742b2ed84b95cf20ac3d149f596673a51ceb3e
Dawar Shamim <DawarShamim1@gmail.com>
2024-05-23 01:19:20 +0500

    updated common functions

--- utils/common.js
+++ utils/common.js
@@ -1,2 +1,22 @@
-exports.throwError(res, message, code, error);
-return res.status(code).json({ success: false, message, error });
+const jwt = require('jsonwebtoken');
+const jwtEncryptionKey = process.env.jwtEncryptionKey;
+
+function successResponse(res, message, code, data = {}) {
+    return res.status(code).json({ success: true, message, data });
+}
+
+function failureResponse(res, message, code, error = {}) {
+    return res.status(code).json({ success: false, message, error });
+};
+
+function getDocumentTotal(totalCount) {
+    return (totalCount && totalCount.length > 0) ? totalCount[0].value : 0
+};
+
+function generateToken(payload, keepMeLogin) {
+    return keepMeLogin ? jwt.sign(payload, jwtEncryptionKey, { expiresIn: '7d' }) : jwt.sign(payload, jwtEncryptionKey, { expiresIn: '12h' });
+}
+
+function generateOTP() {
+    return Math.floor(100000 + Math.random() * 900000);
+}
