c188c642021712d9be7b1374ad0a336b0a03d67e
Dawar Shamim <DawarShamim1@gmail.com>
2024-06-04 01:17:15 +0500

    updated eslint and email service

---
+++ services/sendEmail.js
@@ -0,0 +1,35 @@
+const path = require('path');
+const handlebars = require('handlebars');
+const fs = require('fs');
+
+const { nodeMailerTransporter } = require('../middleware/transporter');
+
+async function sendEmail(
+    userEmail,
+    subject,
+    templateName,
+    emailData = {},
+) {
+    const templatePath = path.resolve(__dirname, `../templates/${templateName}.html`);
+    // need to resolve
+    const templateSource = fs.readFileSync(templatePath,'utf8');
+
+    const template = handlebars.compile(templateSource);
+
+    const html = template(emailData);
+
+    const mailOptions = {
+        from: process.env.EMAIL,
+        to: userEmail,
+        subject: subject,
+        html: html,
+    };
+
+    nodeMailerTransporter.sendMail(mailOptions, (error, info) => {
+        if (error) {
+            console.log(error);
+        } else {
+            console.log('Email sent: ' + info.response);
+        }
+    });
+}
